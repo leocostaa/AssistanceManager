8b539dac20dc66d6bdd13d2be0a05d845d2da288
leocostaa <32634126+leocostaa@users.noreply.github.com>
2023-08-14 16:25:36 +0100

    Layout lateral inicial; Boxes do Layout home; Organização;

---
+++ AssistanceManager/Controllers/AssistancesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AssistanceManager;
+
+namespace AssistanceManager.Controllers
+{
+    public class AssistancesController : Controller
+    {
+        private AssistanceManagerEntities db = new AssistanceManagerEntities();
+
+        // GET: Assistances
+        public ActionResult Index()
+        {
+            var assistance = db.Assistance.Include(a => a.Customers).Include(a => a.Priority1).Include(a => a.AssitanceStatus);
+            return View(assistance.ToList());
+        }
+
+        // GET: Assistances/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assistance assistance = db.Assistance.Find(id);
+            if (assistance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(assistance);
+        }
+
+        // GET: Assistances/Create
+        public ActionResult Create()
+        {
+            ViewBag.Customer = new SelectList(db.Customers, "CustomerID", "Name");
+            ViewBag.Priority = new SelectList(db.Priority, "Priority1", "Priority1");
+            ViewBag.Status = new SelectList(db.AssitanceStatus, "Status", "Status");
+            return View();
+        }
+
+        // POST: Assistances/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AssistanceID,DateTime,Report,Customer,Priority,Status")] Assistance assistance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Assistance.Add(assistance);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Customer = new SelectList(db.Customers, "CustomerID", "Name", assistance.Customer);
+            ViewBag.Priority = new SelectList(db.Priority, "Priority1", "Priority1", assistance.Priority);
+            ViewBag.Status = new SelectList(db.AssitanceStatus, "Status", "Status", assistance.Status);
+            return View(assistance);
+        }
+
+        // GET: Assistances/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assistance assistance = db.Assistance.Find(id);
+            if (assistance == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Customer = new SelectList(db.Customers, "CustomerID", "Name", assistance.Customer);
+            ViewBag.Priority = new SelectList(db.Priority, "Priority1", "Priority1", assistance.Priority);
+            ViewBag.Status = new SelectList(db.AssitanceStatus, "Status", "Status", assistance.Status);
+            return View(assistance);
+        }
+
+        // POST: Assistances/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AssistanceID,DateTime,Report,Customer,Priority,Status")] Assistance assistance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(assistance).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Customer = new SelectList(db.Customers, "CustomerID", "Name", assistance.Customer);
+            ViewBag.Priority = new SelectList(db.Priority, "Priority1", "Priority1", assistance.Priority);
+            ViewBag.Status = new SelectList(db.AssitanceStatus, "Status", "Status", assistance.Status);
+            return View(assistance);
+        }
+
+        // GET: Assistances/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Assistance assistance = db.Assistance.Find(id);
+            if (assistance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(assistance);
+        }
+
+        // POST: Assistances/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Assistance assistance = db.Assistance.Find(id);
+            db.Assistance.Remove(assistance);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AssistanceManager/Controllers/ContractsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AssistanceManager;
+
+namespace AssistanceManager.Controllers
+{
+    public class ContractsController : Controller
+    {
+        private AssistanceManagerEntities db = new AssistanceManagerEntities();
+
+        // GET: Contracts
+        public ActionResult Index()
+        {
+            var contract = db.Contract.Include(c => c.Process1).Include(c => c.Technicians);
+            return View(contract.ToList());
+        }
+
+        // GET: Contracts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contract contract = db.Contract.Find(id);
+            if (contract == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contract);
+        }
+
+        // GET: Contracts/Create
+        public ActionResult Create()
+        {
+            ViewBag.Process = new SelectList(db.Process, "ProcessID", "Description");
+            ViewBag.Technician = new SelectList(db.Technicians, "TechnicianID", "Name");
+            return View();
+        }
+
+        // POST: Contracts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ContractID,Process,Description,DateOfOpen,DateOfClose,Technician,Status")] Contract contract)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contract.Add(contract);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Process = new SelectList(db.Process, "ProcessID", "Description", contract.Process);
+            ViewBag.Technician = new SelectList(db.Technicians, "TechnicianID", "Name", contract.Technician);
+            return View(contract);
+        }
+
+        // GET: Contracts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contract contract = db.Contract.Find(id);
+            if (contract == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Process = new SelectList(db.Process, "ProcessID", "Description", contract.Process);
+            ViewBag.Technician = new SelectList(db.Technicians, "TechnicianID", "Name", contract.Technician);
+            return View(contract);
+        }
+
+        // POST: Contracts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ContractID,Process,Description,DateOfOpen,DateOfClose,Technician,Status")] Contract contract)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contract).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Process = new SelectList(db.Process, "ProcessID", "Description", contract.Process);
+            ViewBag.Technician = new SelectList(db.Technicians, "TechnicianID", "Name", contract.Technician);
+            return View(contract);
+        }
+
+        // GET: Contracts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contract contract = db.Contract.Find(id);
+            if (contract == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contract);
+        }
+
+        // POST: Contracts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contract contract = db.Contract.Find(id);
+            db.Contract.Remove(contract);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
